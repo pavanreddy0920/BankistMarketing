402725decc9b80179f16f31a2d5d6bc1c35265cc
Pavanreddy Bojjireddy <pavanreddy0920@gmail.com>
2022-02-18 19:10:35 +1100

    reveal sections

--- script.js
+++ script.js
@@ -143,3 +143,26 @@ const headerObserver = new IntersectionObserver(sticky, {
 });
 
 headerObserver.observe(header);
+
+////////////////////////////////////////////////////////////////////////////////////
+//////// REVEAL SECTIONS //////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+const sections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+sections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
